5852590aadeaef9e00b737b7908ea7673c29148e
GARIMA <pantgarima12@gmail.com>
2026-01-22 17:54:59 +0530

    database added

--- app/main.py
+++ app/main.py
@@ -58,7 +58,8 @@ async def root():  #async is optional here
 
 app.get("/sqlalchemy")
 def test_posts(db: Session=Depends(get_db)):
-    return{"status":"success"}
+    posts=db.query(models.Post).all()
+    return{"data":posts}
 
 
 @app.get("/posts")
